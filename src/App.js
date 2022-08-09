d73995d55007dec07d0032d4aff20ba44742fd0d
MarkKruhliak <mark25ua@gmail.com>
2022-08-09 15:22:48 +0300

    Project on React

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
-import {getAllMovies, getAllUsers} from "./redux";
-import {useDispatch, useSelector} from "react-redux";
-
-import {AllMovies, Container, User, UserDetails} from "./components";
+import {Container} from "./components";
 
 
 const App = () => {
-    // const [state, setState]= useState([]);
-    const {users} = useSelector(state => state.UserReducer);
-    const {movies} = useSelector(state => state.MovieReducer)
-    const dispatch = useDispatch();
-
-
-    // const getUser = () => {
-    //   UserService.getAll().then(value => setState(value))
-    // }
-
-    // const deleteUser = (id) => {
-    //   let users = [...state]
-    //     users = users.filter(value => value.id !== id)
-    //     setState(users)
-    // }
 
 
     return (
         <div>
             <Container/>
-            {/*{users && users.map(value => <User key={value.id} user={value}/>)}*/}
-            {/*<UserDetails/>*/}
-
-
-            {/*<button onClick={() => dispatch(getAllUsers())}>Apply</button>*/}
-
-            {/*<div>*/}
-            {/*    <button onClick={() => dispatch(getAllMovies())}>GetMovies</button>*/}
-            {/*</div>*/}
-
-            {/*<div>*/}
-            {/*    {movies && movies.map(value => <AllMovies key={value.id} movie={value}/>)}*/}
-            {/*</div>*/}
         </div>
     );
 };
